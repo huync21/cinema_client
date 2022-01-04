d5ffe3a92dcf1a6bcbfdb90ab570491b89cd4ea2
huy <huync21@gmail.com>
2022-01-04 11:38:02 +0700

    search movies by name

--- src/main/java/com/example/cinema_client/constants/Api.java
+++ src/main/java/com/example/cinema_client/constants/Api.java
@@ -1,6 +1,6 @@
 package com.example.cinema_client.constants;
 
 public class Api {
-//    public static String baseURL="https://cinema-be.herokuapp.com/";
-    public static String baseURL="http://localhost:8080";
+    public static String baseURL="https://cinema-be.herokuapp.com/";
+//    public static String baseURL="http://localhost:8080";
 }
